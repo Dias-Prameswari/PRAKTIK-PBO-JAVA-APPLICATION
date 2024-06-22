ceeaf0cbe8aab81c7f21d8b1f8015b4c85e9ef68
Dias-Prameswari <114818845+Dias-Prameswari@users.noreply.github.com>
2024-06-23 00:32:19 +0700

    Add files via upload

---
+++ PRAKTIKUM9PBO/src/main/java/com/mycompany/praktikum9pbo/Account.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikum9pbo;
+
+/**
+ *
+ * @author HP
+ */
+public class Account {
+    protected double balance;
+    protected Account(double initbalance) {
+        balance = initbalance;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void deposit(double amt){
+        balance = balance + amt;
+    }
+    
+    public void withdraw(double amt) throws OverdraftException {
+        if(amt <= balance){
+            balance = balance + amt;
+        }else{
+            throw new OverdraftException("Dana Anda tidak cukup", amt - balance);
+        }
+    }
+}
---
+++ PRAKTIKUM9PBO/src/main/java/com/mycompany/praktikum9pbo/PRAKTIKUM9PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum9pbo;
+
+/**
+ *
+ * @author HP
+ */
+public class PRAKTIKUM9PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
